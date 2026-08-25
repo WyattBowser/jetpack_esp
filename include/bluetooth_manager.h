9bb1a798de835840bac7e8ff67c60de27b8a2953
Wyatt Bowser <wyatthbowser@gmail.com>
2026-08-25 12:55:25 -0600

    Bluetooth connects

---
+++ include/bluetooth_manager.h
@@ -0,0 +1,28 @@
+#include <ArduinoBLE.h>
+#include <jetpack_state.h>
+#include <state_processor.h>
+
+class StateProcessor;
+
+enum ConnectionState {
+  CONNECTED,
+  DISCONNECTED
+};
+
+class BluetoothManager {
+public:
+  BluetoothManager(StateProcessor& state_proc, JetpackState& state);
+  void init();
+  void process();
+
+private:
+  const char* LOCAL_NAME = "Hijack Jetpack";
+  const char* CHARACTERISTIC_UUID = "6969";
+  BLEIntCharacteristic control_characteristic;
+  BLEDevice central;
+
+  const StateProcessor& state_processor;
+  const JetpackState& current_state;
+  
+  ConnectionState last_connection_state;
+};
---
+++ src/bluetooth_manager.cpp
@@ -0,0 +1,66 @@
+#include "bluetooth_manager.h"
+
+BluetoothManager::BluetoothManager(StateProcessor& state_proc, JetpackState& state) :
+  state_processor(state_proc), current_state(state), control_characteristic(CHARACTERISTIC_UUID, BLERead | BLEWrite) {
+    last_connection_state = DISCONNECTED;
+}
+
+void BluetoothManager::init() {
+  BLE.begin();
+  delay(250);
+  BLE.setLocalName(LOCAL_NAME);
+  Serial.print("Local Name set to: ");
+  Serial.println(LOCAL_NAME);
+
+  BLEService new_service("69420");
+  Serial.print("Service created: ");
+  Serial.println(new_service.uuid());
+
+  new_service.addCharacteristic(control_characteristic);
+  Serial.print("Service now has characteristic: ");
+  Serial.println(new_service.characteristic("6969").uuid());
+
+  BLE.setAdvertisedService(new_service);
+  BLE.addService(new_service);
+  control_characteristic.writeValue((uint8_t)-1);
+  BLE.advertise();
+  
+  Serial.println(new_service.characteristicCount());
+}
+
+void BluetoothManager::process() {
+  if (!central) {
+    central = BLE.central();
+  } else {
+    if (central.connected()) {
+      if (last_connection_state != CONNECTED) {
+        Serial.println("Jetpack BT is now connected to bracer");
+        last_connection_state = CONNECTED;
+      }
+
+      if (control_characteristic.written()) {
+        int control = (int)control_characteristic.value();
+        switch (control) {
+          case JetpackState::SPOOLING_UP:
+            Serial.println("Jetpack BT got command to spool up");
+            //Start full sequence
+            break;
+          case JetpackState::SPOOLING_DOWN:
+            Serial.println("Jetpack BT got command to spool down");
+            //Start spooldown sequenc
+            break;
+          case JetpackState::DISARMED:
+            Serial.println("Jetpack BT got command to force stop");
+            //hard stop everything
+            break;
+        }
+      }
+    } else {
+      if (last_connection_state != DISCONNECTED) {
+        Serial.println("Jetpack BT Disconnected from Bracer");
+        last_connection_state = DISCONNECTED;
+      }
+      //Terminate everything for safety
+    }
+  }
+}
